14702f38befb46b28fee6f30cc952ae37017e92e
AugustoTI <augustocesar4001@gmail.com>
2025-01-02 19:06:49 -0300

    feat: setup a basic server

---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+  PORT: z.coerce.number().default(3000),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('❌ Invalid Environment Variables', _env.error.format())
+
+  throw new Error('Invalid Environment Variables')
+}
+
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Teste')
+import { app } from './app'
+import { env } from './lib/env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`HTTP Server Running: http://0.0.0.0:${env.PORT}`)
+  })
