f4057b6deea67364e22961f4fa4af50b94d3ab01
AugustoTI <augustocesar4001@gmail.com>
2025-01-03 18:47:37 -0300

    feat: add basic route for create new user

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import { fastify } from 'fastify'
+import { z } from 'zod'
+import { db } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (req, res) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return res.status(201).send()
+})
