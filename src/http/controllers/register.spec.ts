5c0320f582768de2c90b5db8c2dfaf622e4c10a5
AugustoTI <augustocesar4001@gmail.com>
2025-01-26 20:42:05 -0300

    test: write tests e2e for authenticate and register controllers

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import { agent } from 'supertest'
 
-test('ok', () => {
-  // ...
+import { app } from '@/app'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await agent(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
